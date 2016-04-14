7d0a1118432b01b820eb0cfb0f66ed99013a8177
SeanMcGrath <srmcgrat@umass.edu>
2016-04-14 13:46:46 -0400

    done

--- err_analysis.py
+++ err_analysis.py
@@ -6,9 +6,12 @@ import random
 import seaborn as sns
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.optimize import minimize
+from scipy.optimize import fsolve
+from scipy.interpolate import interp1d
 
 from resources import *
+
+sns.set(context='poster', style='white', font_scale=1.2)
 
 try:
 	num_decays = int(sys.argv[1])
@@ -22,15 +25,18 @@ def add1(x):
 	else:
 		return x
 
-def chisq(ydata,ymod,sd=None):  
+def chisq(ydata,ymod,sd=None, ddof=None):  
 
-      # Chi-square statistic (Bevington, eq. 6.9)  
-      if sd is None:  
-           chisq=np.sum((ydata-ymod)**2)  
-      else:  
-           chisq=np.sum( ((ydata-ymod)/sd)**2 )  
-        
-      return chisq  
+	# Chi-square statistic (Bevington, eq. 6.9)  
+	if sd is None:
+		chisq=np.sum((ydata-ymod)**2)  
+	else:
+		chisq=np.sum( ((ydata-ymod)/sd)**2 )
+
+	if ddof is None:
+		return chisq
+	else:
+		return chisq/(len(ydata) - ddof - 1)
 
 ref_x = np.arange(1,8)
 ref_data = np.array([0, 0, 4, 7, 13, 15, 4])
@@ -39,7 +45,6 @@ possible_r = np.arange(0, R/2, 0.001)
 
 def monte(muon_mass):
 	# generate random energies
-	print(muon_mass)
 	energy_pdf = []
 	energies = []
 	while len(energies) < num_decays:
@@ -62,13 +67,27 @@ def monte(muon_mass):
 
 	hist = np.histogram(sparks, bins=[el-0.5 for el in range(1, 9)])[0]
 	hist = hist * sum(ref_data)/num_decays
-	return chisq(ref_data, hist, ref_err)
+	return chisq(ref_data, hist, ref_err, 2)
 
 results = []
-for i in range(2):
-	res = minimize(monte, x0=(95), method='powell', options={'disp': True})
-	print(res.x)
-	results.append(res.x)
+masses = np.linspace(80, 120, 200)
+chis = [monte(mass) for mass in masses]
+interp = interp1d(masses, chis)
+x_arr = np.linspace(min(masses), max(masses), 1000)
+y_arr = [interp(x) for x in x_arr]
+min_y = min(y_arr)
+plt.plot(x_arr, y_arr, 'k')
+plt.plot((0, 200), (min_y+1, min_y+1), 'k--')
+plt.plot((0, 200), (min_y, min_y), 'k--')
+plt.ylabel('Reduced $\chi^2$')
+plt.xlabel('Muon Mass (MeV)')
+plt.xlim(85, 115)
+plt.ylim(0.2, 2.2)
+min_mass = x_arr[y_arr.index(min_y)]
+x = fsolve(lambda x: interp(x) - (min_y +1), x0=(90, 110))
+print('minimum mass:\t', min_mass)
+print('extrema:\t', x[0], x[1])
+plt.show()
+	
 
 print(np.mean(results))
-print(np.std(results))
